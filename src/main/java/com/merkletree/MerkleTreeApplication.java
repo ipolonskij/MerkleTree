6515303b73c13d26bf510107efe655bb194bd1ef
iljapolonskij <ilja.polonskij@gmail.com>
2023-06-02 15:47:43 +0200

    Initial commit

---
+++ src/main/java/com/merkletree/MerkleTreeApplication.java
@@ -0,0 +1,15 @@
+package com.merkletree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerkleTreeApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MerkleTreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/merkletree/MerkleTreeApplicationTests.java
@@ -0,0 +1,15 @@
+package com.merkletree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerkleTreeApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
